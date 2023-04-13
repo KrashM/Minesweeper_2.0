b1ee5c1853d2b7d5a7f89e77e9a15463ef8828d3
KrashM <hristo_mesi1990@abv.bg>
2023-04-14 00:49:44 +0300

    Removed

--- src/Cell.cpp
+++
@@ -1,7 +0,0 @@
-#include "../headers/cell.hpp"
-
-cell::cell(): _value(UNCOVERED){}
-cell::cell(const u_char value): _value(value){}
-
-u_char cell::get_value() const{ return this -> _value; }
-void cell::set_value(u_char const value){ this -> _value = value; }
