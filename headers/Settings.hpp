2a2029d8d23946e4774871c8ca3407cf7221c56b
KrashM <hristo_mesi1990@abv.bg>
2023-04-13 21:13:54 +0300

    Updated to be as singleton, rather than all static

--- headers/Settings.hpp
+++ headers/Settings.hpp
@@ -1,29 +1,91 @@
 #pragma once
 
-#include "Types.hpp"
+#include <cstdint>
+#include "out_of_bounds.hpp"
+#include "../utils/debug_new.h"
 
-class Settings{
+typedef unsigned char u_char;
 
-    public:
-        Settings(const u_short, const u_short, const u_short);
+enum class difficulty{
 
-        const u_short getHeight() const;
-        const u_short getWidth() const;
-        const u_short getBombs() const;
-
-        static const Settings EASY_8x8;
-        static const Settings EASY_9x9;
-        static const Settings EASY_10x10;
-
-        static const Settings INTERMEDIATE_14x14;
-        static const Settings INTERMEDIATE_15x15;
-        static const Settings INTERMEDIATE_16x16;
-
-        static const Settings EXPERT_20x20;
-        static const Settings EXPERT_22x22;
-        static const Settings EXPERT_24x24;
-    
-    private:
-        u_short _height, _width, _bombs;
+    EASY,
+    MEDIUM,
+    HARD
 
 };
+
+class settings{
+
+    public:
+        ~settings() = default;
+
+        settings(settings const &) = delete;
+        settings(settings &&) = delete;
+
+        settings &operator =(settings const &) = delete;
+        settings &operator =(settings &&) = delete;
+
+        static settings &get_instance();
+        static void destroy_instance();
+
+        void set_difficulty(difficulty const);
+
+        uint16_t const get_height() const;
+        uint16_t const get_width() const;
+        uint16_t const get_bombs() const;
+    
+    private:
+        settings() = default;
+
+        uint16_t _height, _width, _bombs;
+
+};
+
+#ifdef DEBUG_NEW
+#define new new(1)
+#endif
+
+#ifdef DEBUG
+#ifndef DOCTEST_LIBRARY_INCLUDED
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "../utils/doctest.h"
+#endif
+
+TEST_SUITE("Testing different difficulties"){
+
+    TEST_CASE("Testing easy difficulty"){
+
+        settings &instance = settings::get_instance();
+        instance.set_difficulty(difficulty::EASY);
+
+        CHECK_EQ(instance.get_width(), 9);
+        CHECK_EQ(instance.get_height(), 9);
+        CHECK_EQ(instance.get_bombs(), 10);
+
+    }
+
+    TEST_CASE("Testing medium defficulty"){
+        
+        settings &instance = settings::get_instance();
+        instance.set_difficulty(difficulty::MEDIUM);
+
+        CHECK_EQ(instance.get_width(), 16);
+        CHECK_EQ(instance.get_height(), 16);
+        CHECK_EQ(instance.get_bombs(), 40);
+
+    }
+
+    TEST_CASE("Testing hard difficulty"){
+
+        settings &instance = settings::get_instance();
+        instance.set_difficulty(difficulty::HARD);
+
+        CHECK_EQ(instance.get_width(), 30);
+        CHECK_EQ(instance.get_height(), 16);
+        CHECK_EQ(instance.get_bombs(), 99);
+
+    }
+
+}
+
+#endif
--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,25 +1,39 @@
-#include "../headers/Settings.hpp"
+#include "../headers/settings.hpp"
 
-#define EASY_BOMBS 10
-#define INTERMEDIATE_BOMBS 40
-#define EXPERT_BOMBS 99
+settings &settings::get_instance(){
 
-enum EASY_DIMS{ EIGHT = 8, NINE, TEN };
-enum INTERMEDIATE_DIMS{ FOURTEEN = 14, FIFTEEN, SIXTEEN };
-enum EXPERT_DIMS{ TWENTY = 20, TWENTY_TWO = 22, TWENTY_FOUR = 24 };
+    static settings instance;
+    return instance;
 
-const Settings Settings::EASY_8x8(EASY_DIMS::EIGHT, EASY_DIMS::EIGHT, EASY_BOMBS);
-const Settings Settings::EASY_9x9(EASY_DIMS::NINE, EASY_DIMS::NINE, EASY_BOMBS);
-const Settings Settings::EASY_10x10(EASY_DIMS::TEN, EASY_DIMS::TEN, EASY_BOMBS);
-const Settings Settings::INTERMEDIATE_14x14(INTERMEDIATE_DIMS::FOURTEEN, INTERMEDIATE_DIMS::FOURTEEN, INTERMEDIATE_BOMBS);
-const Settings Settings::INTERMEDIATE_15x15(INTERMEDIATE_DIMS::FIFTEEN, INTERMEDIATE_DIMS::FIFTEEN, INTERMEDIATE_BOMBS);
-const Settings Settings::INTERMEDIATE_16x16(INTERMEDIATE_DIMS::SIXTEEN, INTERMEDIATE_DIMS::SIXTEEN, INTERMEDIATE_BOMBS);
-const Settings Settings::EXPERT_20x20(EXPERT_DIMS::TWENTY, EXPERT_DIMS::TWENTY, EXPERT_BOMBS);
-const Settings Settings::EXPERT_22x22(EXPERT_DIMS::TWENTY_TWO, EXPERT_DIMS::TWENTY_TWO, EXPERT_BOMBS);
-const Settings Settings::EXPERT_24x24(EXPERT_DIMS::TWENTY_FOUR, EXPERT_DIMS::TWENTY_FOUR, EXPERT_BOMBS);
+}
 
-Settings::Settings(const u_short height, const u_short width, const u_short bombs): _height(height), _width(width), _bombs(bombs){}
+void settings::set_difficulty(difficulty const diff){
 
-const u_short Settings::getHeight() const{ return this -> _height; }
-const u_short Settings::getWidth() const{ return this -> _width; }
-const u_short Settings::getBombs() const{ return this -> _bombs; }
+    switch(diff){
+
+        case difficulty::EASY:
+            _height = _width = 9;
+            _bombs = 10;
+            break;
+
+        case difficulty::MEDIUM:
+            _height = _width = 16;
+            _bombs = 40;
+            break;
+
+        case difficulty::HARD:
+            _height = 16;
+            _width = 30;
+            _bombs = 99;
+            break;
+        
+        default:
+            break;
+
+    }
+
+}
+
+uint16_t const settings::get_height() const{ return _height; }
+uint16_t const settings::get_width() const{ return _width; }
+uint16_t const settings::get_bombs() const{ return _bombs; }
