714c46e337a04557856e311da3d2e19a7d57f3c2
KrashM <hristo_mesi1990@abv.bg>
2023-04-14 23:05:26 +0300

    Added debug config file

---
+++ headers/debug.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+// #define DEBUG
+#define DEBUG_NEW
+
+#ifdef DEBUG_NEW
+#include "../utils/debug_new.h"
+#define new new(1)
+#endif
+
+#ifdef DEBUG
+#ifndef DOCTEST_LIBRARY_INCLUDED
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "../utils/doctest.h"
+#endif
+#endif
+
+#ifdef DEBUG
+#define test doctest::Context().run()
+#else
+#define test
+#endif
+
+#ifdef DEBUG_NEW
+#define kill_singletons settings::get_instance().~settings();\
+board::get_instance().~board()
+#define leaks allocator::get_instance().checkMemoryLeaks()
+#else
+#define leaks
+#endif
