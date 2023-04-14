3f80ad193fe116d8b572a3d3a69f38812ddcb9b6
KrashM <hristo_mesi1990@abv.bg>
2023-04-14 15:57:41 +0300

    Testing with main entry

--- Program.cpp
+++ Program.cpp
@@ -1,6 +1,7 @@
-#define DEBUG
-// #define DEBUG_NEW
+// #define DEBUG
+#define DEBUG_NEW
 #include "headers/settings.hpp"
+#include "headers/board.hpp"
 
 #include <iostream>
 
@@ -10,12 +11,28 @@
 #define test
 #endif
 
+#ifdef DEBUG_NEW
+#define leaks checkMemoryLeaks()
+#else
+#define leaks
+#endif
+
 using std::cout;
 
 int main(){
 
     test;
 
-    checkMemoryLeaks();
+    settings &settings_instance = settings::get_instance();
+    settings_instance.set_difficulty(difficulty::EASY);
+
+    board &board_instance = board::get_instance();
+    for(uint16_t i = 0; i < settings_instance.get_height(); ++i)
+        for(uint16_t j = 0; j < settings_instance.get_width(); ++j)
+            board_instance.review_cell({i, j});
+
+    cout << board_instance << '\n';
+
+    leaks;
 
 }
