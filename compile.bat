f8237920a7ba40f0a92c75b376102a795efc6a79
KrashM <hristo_mesi1990@abv.bg>
2023-04-30 17:36:10 +0300

    Changed compilation procedure

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 @echo off
-g++ -o main.exe Program.cpp src/*.cpp
+
+g++ Program.cpp -I C:/Libraries/SFML/include -L C:/Libraries/SFML/lib -l sfml-graphics-s -l sfml-window-s -l sfml-system-s -l opengl32 -l freetype -l winmm -l gdi32 -D SFML_STATIC -o main.exe
 main.exe
 del main.exe
