95e5f98ed56cea612dd932f79a731007704f85b7
KrashM <hristo_mesi1990@abv.bg>
2023-05-02 19:24:11 +0300

    Redone to work with new compilation task

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 @echo off
 
-g++ Program.cpp -I C:/Libraries/SFML/include -L C:/Libraries/SFML/lib -l sfml-graphics-s -l sfml-window-s -l sfml-system-s -l opengl32 -l freetype -l winmm -l gdi32 -D SFML_STATIC -o main.exe
-main.exe
-del main.exe
+set batch_dir=%~dp0
+g++ %batch_dir%\Program.cpp %batch_dir%\src\*.cpp -I C:\Libraries\SFML\include -L C:\Libraries\SFML\lib -l sfml-graphics-s -l sfml-window-s -l sfml-system-s -l opengl32 -l freetype -l winmm -l gdi32 -D SFML_STATIC -o %batch_dir%\main.exe
+%batch_dir%\main.exe
+del %batch_dir%\main.exe
